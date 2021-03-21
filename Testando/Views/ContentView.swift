5bc9b8be1026108530a86040836fd13ce581f6c3
Ricardo Nogueira <seuze.rick@gmail.com>
2021-03-21 18:05:39 -0300

    :sparkles: it adds navigation between landmarks :sparkles: it adds dynamic rendering to landmark details

--- Testando/Views/ContentView.swift
+++ Testando/Views/ContentView.swift
@@ -9,33 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            MapView()
-                .frame(height: 200)
-            CircleImage()
-                .offset(y: -130)
-                .padding(EdgeInsets(top: 0, leading: 10, bottom: -130, trailing: 0))
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-
-                Divider()
-
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            Spacer()
-        }
-            
+        LandmarkList()
     }
 }
 
